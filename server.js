d4e3245fcb812107894eab99d54cc5311aae358c
thitoribeiro <thito.ribeiro@hotmail.com>
2021-12-29 16:50:15 -0300

    Creating a basic web server and port normalize

--- server.js
+++ server.js
@@ -1,5 +1,63 @@
 'use strict'
 
-const http =  require('http');
+const http = require('http');
 const debug = require('debug')('node-api-course-baltaio:server');
 const express = require('express');
+
+const app = express();
+const port = 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+const router = express.Router();
+
+const route = router.get('/', (req, res, next) => {
+    res.status(200).send({
+        title: "Node Store API",
+        version: "0.0.1"
+    });
+});
+
+app.use('/', route);
+
+server.listen(port);
+server.on('error', onError);
+
+console.log('API running at port ' + port);
+
+function normalizePort(val) {
+    const port = parseInt(val, 10);
+
+    if(isNaN(port)) {
+        return val;
+    }
+
+    if (port >= 0) {
+        return port;
+    }
+
+    return false;
+}
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    const bind = typeof port === 'string' ?
+        'Pipe ' + port :
+        'Pipe ' + port;
+
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
