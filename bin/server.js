c2b88673e6e226038c0a269e2375591ae4dfc15b
thitoribeiro <thito.ribeiro@hotmail.com>
2021-12-29 17:44:23 -0300

    Separating the responsabilities in two files

--- bin/server.js
+++ bin/server.js
@@ -1,24 +1,12 @@
-'use strict'
-
-const http = require('http');
+const app = require('../src/app');
 const debug = require('debug')('node-api-course-baltaio:server');
-const express = require('express');
+const http = require('http');
 
-const app = express();
-const port = 3000;
+
+const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 const server = http.createServer(app);
-const router = express.Router();
-
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: "Node Store API",
-        version: "0.0.1"
-    });
-});
-
-app.use('/', route);
 
 server.listen(port);
 server.on('error', onError);
