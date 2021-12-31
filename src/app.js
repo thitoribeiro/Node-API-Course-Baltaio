bf84c73fe98e7eae4eed26295b8fa73e6af0a44d
thitoribeiro <thito.ribeiro@hotmail.com>
2021-12-31 09:46:20 -0300

    Incrementing routes

--- src/app.js
+++ src/app.js
@@ -7,30 +7,15 @@ const app = express();
 const router = express.Router();
 
 //Carrega as Rotas
-const index = require('./routes/index');
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ 
+    extended: false 
+}));
 
-const create = router.post('/', (req, res, next) => {
-    res.status(201).send(req.body);
-});
-
-const put = router.put('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(201).send({ 
-        id: id, 
-        item: req.body 
-    });
-});
-
-const del = router.delete('/', (req, res, next) => {
-    res.status(200).send("Sucessful deleted!");
-});
-
-app.use('/', index);
-app.use('/products', create);
-app.use('/products', put);
-app.use('/products', del);
+app.use('/', indexRoute);
+app.use('/products', productRoute);
 
 module.exports = app;
