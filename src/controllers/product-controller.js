7ff417f86c133a4945951e8d1209723f1be7ad50
thitoribeiro <thito.ribeiro@hotmail.com>
2022-01-05 08:18:23 -0300

    PUT method updanting products by id

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -71,13 +71,25 @@ exports.post = (req, res, next) => {
     
 };
 
-exports.put = ('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(201).send({ 
-        id: id, 
-        item: req.body 
+exports.put = (req, res, next) => {
+    Product
+        .findByIdAndUpdate(req.params.id, {
+            $set: {
+                title: req.body.title,
+                description: req.body.description,
+                slug: req.body.slug,
+                price: req.body.price
+            }
+        }).then(x => {
+                res.status(200).send({ message: 'Produto atualizado com sucesso!'});
+        
+        }).catch(e => {
+                res.status(400).send({ message: 'Falha ao atualizar produto', 
+                data: e
+        });
+        
     });
-});
+};
 
 exports.delete = ('/', (req, res, next) => {
     res.status(200).send(req.body);
