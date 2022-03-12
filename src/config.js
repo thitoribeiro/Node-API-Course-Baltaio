4c682acc91885768ba9e1775e03765dbdb290767
thitoribeiro <thito.ribeiro@hotmail.com>
2022-03-12 16:05:44 -0300

    Files review

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Orders!'
 
 module.exports = {
     connectionString: 'mongodb+srv://thito:senha123@mongostudy.0tawc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    sendgridKey: 'sendgridKey',
-    containerConnectionString: 'TBD'
+    sendgridKey: 'put-sendgridKey-here',
+    containerConnectionString: 'DefaultEndpointsProtocol=https;AccountName=nodeorder;AccountKey=7qyrk4emln08W4pYC/tgxggpKR09jWnMdBBjiUqyzEuQgEoZNvEW+AIULNjS55SLlkvH1zvfDsRv47nH+aCcyA==;EndpointSuffix=core.windows.net'
 }
--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -2,6 +2,8 @@
 
 const ValidationContract = require('../validators/fluent-validator');
 const repository = require('../repositories/product-repository');
+const azure = require('azure-storage');
+const guid = require('quid');
 
 exports.get = async(req, res, next) => {
     try {
@@ -51,6 +53,26 @@ exports.getByTag = async(req, res, next) => {
 
 exports.post = async (req, res, next) => {
    try {
+
+        //Create Blob Service
+        const blobSvc = azure.createBlobService(config.userImagesBlobConnectionString);
+
+        let filename = guid.raw().toString() + '.jpg';
+        let rawdata = req.body.image;
+        let matches = rawdata.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+        let type = matches[1];
+        let buffer = new BufferEncoding(matches[2], 'base64');
+
+        //Save image
+        blobSvc.createBlockBlobFromText('product-images', filename, buffer, {
+           contentType: type
+       }, function (error, result, response) {
+           it(error); {
+               filename = 'default-product.png';
+           }
+       });
+
+
         await repository.create(req.body);
         res.status(201).send({ 
             message: 'Produto cadastrado com sucesso!'
